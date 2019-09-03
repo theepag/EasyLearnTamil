0df03f2a615ff9d31315d278d03d11bd483d9448
Theepag <theepag@live.com>
2019-09-03 11:04:20 +0530

    total score descending ordered successfully

--- app/src/main/java/com/example/myapp/HighScore.java
+++ app/src/main/java/com/example/myapp/HighScore.java
@@ -20,22 +20,44 @@ public class HighScore extends AppCompatActivity {
         final int[] level1= new int[400];
         final int[] level2= new int[400];
         final int[] level3= new int[400];
-        final int[] total= new int[400];
+        final int[][] total= new int[400][2];
 
         int i=0;
+        int j=0;
         while(cursor.moveToNext()){
             //   stringBuilder.append(cursor.getString(2));
 
-            id[i] = cursor.getInt(0);
+
             level1[i] = cursor.getInt(1);
             level2[i] = cursor.getInt(2);
             level3[i] = cursor.getInt(3);
-            total[i] = level1[i] + level2[i] + level3[i];
 
+            total[i][0] = level1[i] + level2[i] + level3[i];
+            total[i][1] = cursor.getInt(0);
             i++;
+
         }
 
-        Toast.makeText(getApplicationContext(),"updated "+ id[1] + " " + level1[1]+" "+ total[1]+" "+ level3[1],Toast.LENGTH_SHORT).show();
+        int temp,temp2;
+        for (int k =0; k < 400; k++) {
+            for (int l = k+ 1; l < 400; l++ ) {
+                if (total[k][0] < total[l][0])
+                {
+                    temp = total[k][0];
+                    temp2 = total[k][1];
+
+                    total[k][0] = total[l][0];
+                    total[k][1] = total[l][1];
+                    total[l][0] = temp;
+                    total[l][1] = temp2;
+
+                }
+
+            }
+        }
+
+
+        Toast.makeText(getApplicationContext(),"updated " + total[0][0]+" "+ total[0][1] +" "+total[1][0]+" "+ total[1][1] ,Toast.LENGTH_SHORT).show();
 
 
 
