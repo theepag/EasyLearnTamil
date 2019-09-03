8442c370b1be6c1797c280796fd1d27f05fdf3d2
Theepag <theepag@live.com>
2019-09-03 22:59:52 +0530

    high score half added

--- app/src/main/java/com/example/myapp/HighScore.java
+++ app/src/main/java/com/example/myapp/HighScore.java
@@ -3,16 +3,25 @@ package com.example.myapp;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class HighScore extends AppCompatActivity {
     DatabaseHelper db;
+    TextView name_one,score_one,name_two,score_two;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
+
+        name_one = findViewById(R.id.name_one);
+        name_two = findViewById(R.id.name_two);
+        score_one = findViewById(R.id.score_one);
+        score_two = findViewById(R.id.score_two);
+
+
         db=new DatabaseHelper(this);
         Cursor cursor=  db.viewHighScore();
         StringBuilder stringBuilder=new StringBuilder();
@@ -45,19 +54,59 @@ public class HighScore extends AppCompatActivity {
                 {
                     temp = total[k][0];
                     temp2 = total[k][1];
-
                     total[k][0] = total[l][0];
                     total[k][1] = total[l][1];
                     total[l][0] = temp;
                     total[l][1] = temp2;
-
                 }
 
             }
         }
 
+        Cursor cursr=  db.getUser();
+        StringBuilder stringBuildr=new StringBuilder();
 
-        Toast.makeText(getApplicationContext(),"updated " + total[0][0]+" "+ total[0][1] +" "+total[1][0]+" "+ total[1][1] ,Toast.LENGTH_SHORT).show();
+        int z=0;
+        final int[] userID= new int[400];
+        final String[] name= new String[400];
+        final String[] nameSorted= new String[400];
+
+
+        while(cursr.moveToNext()){
+
+            userID[z] = cursr.getInt(0);
+            name[z] = cursr.getString(1);
+
+            z++;
+
+        }
+        for (int a= 0; a < 400; a++) {
+
+            for (int b= 0; b < 400; b++) {
+                if (total[a][1] == userID[b]) {
+
+                    nameSorted[a] = name[b];
+
+                }
+
+            }
+
+        }
+
+
+        name_one.setText(total[1][0]);
+        score_one.setText(nameSorted[0]);
+        name_two.setText(total[2][0]);
+        score_two.setText(nameSorted[1]);
+
+
+
+
+
+
+
+
+      //  Toast.makeText(getApplicationContext(),"updated " +total[1][0]+" "+ nameSorted[0]+" "+ total[2][0]+ " "+nameSorted[1] ,Toast.LENGTH_SHORT).show();
 
 
 
